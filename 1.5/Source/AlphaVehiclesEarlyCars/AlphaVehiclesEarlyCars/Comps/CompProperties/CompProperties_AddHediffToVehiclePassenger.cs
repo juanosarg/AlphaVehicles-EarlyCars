d0dd8533d914c67261dfaae840bd229fb91f4e38
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-09-19 19:01:49 +0200

    v0.1

---
+++ 1.5/Source/AlphaVehiclesEarlyCars/AlphaVehiclesEarlyCars/Comps/CompProperties/CompProperties_AddHediffToVehiclePassenger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using RimWorld;
+using UnityEngine;
+using Vehicles;
+
+
+namespace AlphaVehiclesEarlyCars
+{
+   
+    public class CompProperties_AddHediffToVehiclePassenger : VehicleCompProperties
+    {
+        public List<HediffToRoles> hediffsToAdd;
+
+        public CompProperties_AddHediffToVehiclePassenger()
+        {
+            compClass = typeof(CompAddHediffToVehiclePassenger);
+        }
+
+     
+    }
+}
---
+++ 1.5/Source/AlphaVehiclesEarlyCars/AlphaVehiclesEarlyCars/Harmony/HarmonyInstance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using System.Reflection;
+
+namespace AlphaVehiclesEarlyCars
+{
+
+    //Setting the Harmony instance
+    [StaticConstructorOnStartup]
+    public class MainEarlyCars
+    {
+        static MainEarlyCars()
+        {
+            var harmony = new Harmony("com.alphavehiclesearlycars");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+
+}
